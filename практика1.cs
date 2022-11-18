c0b1e78f9117ce0ae29440101c6d4b5a192bf607
katirina233 <118421008+katirina233@users.noreply.github.com>
2022-11-18 13:36:45 +0900

    Add files via upload

---
+++ практика1.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b = 0;
+        Console.WriteLine("Введите первое число:");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число:");
+        b = int.Parse(Console.ReadLine());
+        Subst.Math(a, b);
+        Console.ReadKey();
+    }
+}
+class Subst
+{
+    public static void Math(int a, int b)
+    {
+        Console.WriteLine("Сумма равна:{0}", a + b);
+    }
+}
